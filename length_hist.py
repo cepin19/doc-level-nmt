250917f5e410188713c33590eb86bd6da1fad795
Your Name <pepa@cosmas.lingea.cz>
2018-11-27 16:29:42 +0100

    length histograms

---
+++ length_hist.py
@@ -0,0 +1,13 @@
+import matplotlib.pyplot as plt
+import sys
+import numpy as np
+
+lengths = [len(line.split(' ')) for line in sys.stdin.readlines()]
+print (sorted(lengths)[-10:])
+if len(sys.argv)>1:
+    longer=np.where(np.asarray(lengths)>int(sys.argv[1]))[0].shape[0]
+    print ("number of lines longer than %s: %s (%s)"%(sys.argv[1],longer,float(longer)/len(lengths)))
+plt.hist(lengths, bins=np.arange(max(lengths)), histtype='step', linewidth=1);
+plt.show()
+
+

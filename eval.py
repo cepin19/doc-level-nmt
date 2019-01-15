0c08413340101645ea8d3812cd11c123d3eab0c1
cepin <cepin19@gmail.com>
2019-01-15 19:49:28 +0100

    eval update

--- eval.py
+++ eval.py
@@ -10,18 +10,20 @@ tok_tgt=prepost.Tokenizer(tgt)
 true_src=prepost.TrueCaserMoses("/home/big_maggie/data/corp/tcs_models/truecase-model.%s"%src)
 true_tgt=prepost.TrueCaserMoses("/home/big_maggie/data/corp/tcs_models/truecase-model.%s"%tgt)
 examples=[]
-bpe_src=prepost.Segmentator("enfr.bpe")
-bpe_tgt=prepost.Segmentator("enfr.bpe")
+bpe_src_opensub=prepost.Segmentator("enfr.opensub.bpe")
+bpe_tgt_opensub=prepost.Segmentator("enfr.opensub.bpe")
+bpe_src_europarl=prepost.Segmentator("enfr.europarl.bpe")
+bpe_tgt_europarl=prepost.Segmentator("enfr.europarl.bpe")
 def preprocess(line,dir):
     if dir=="src":
-        return bpe_src(true_src(tok_src(line)))
+        return (bpe_src_europarl(true_src(tok_src(line))),bpe_src_opensub(true_src(tok_src(line))))
     else:
-        return bpe_tgt(true_tgt(tok_tgt(line)))
+        return (bpe_tgt_europarl(true_tgt(tok_tgt(line))),bpe_tgt_opensub(true_tgt(tok_tgt(line))))
 def score(config,src,correct,incorrect):
     subprocess.call("{0}/marian-scorer -c {1} -t {2} {3}".format(marian_path,config,src,correct))
     subprocess.call("{0}/marian-scorer -c {1} -t {2} {3}".format(marian_path,config,src,incorrect))
 
-with open("coherence-cohesion.json") as cctest, open("coherence-cohesion.src","w") as src, open("coherence-cohesion.correct","w") as correct, open("coherence-cohesion.incorrect","w") as incorrect:
+with open("coherence-cohesion.json") as cctest, open("coherence-cohesion.europarl.src","w") as srcEuroparl,open("coherence-cohesion.europarl.opensub.src","w") as srcOpensub, open("coherence-cohesion.europarl.correct","w") as correctEuroparl,open("coherence-cohesion.opensub.correct","w") as correctOpensub, open("coherence-cohesion.europarl.incorrect","w") as incorrectEuroparl, open("coherence-cohesion.opensub.incorrect","w") as incorrectOpensub:
     ccexamples=json.load(cctest)
     for i in ccexamples:
         print("Example %s:\n"%i)
@@ -30,7 +32,13 @@ with open("coherence-cohesion.json") as cctest, open("coherence-cohesion.src","w
             print (example["src"])
             print (example["trg"]["incorrect"])
             print (example["trg"]["correct"])
-            src.write(" <context> ".join((preprocess(example["src"][0],"src"),preprocess(example["src"][1],"src"))))
-            incorrect.write(preprocess(example["trg"]["incorrect"][1],"tgt"))
-            correct.write(preprocess(example["trg"]["correct"][1],"tgt"))
+            #srcText=#tuple (europarl, opensub), different BPE
+            srcEuroparl.write(" <context> ".join((preprocess(example["src"][0],"src")[0],preprocess(example["src"][1],"src")[0])))
+            srcOpensub.write(" <context> ".join((preprocess(example["src"][0],"src")[1],preprocess(example["src"][1],"src")[1])))
+            incorrectEuroparl.write(preprocess(example["trg"]["incorrect"][1],"tgt")[0])
+            incorrectOpensub.write(preprocess(example["trg"]["incorrect"][1],"tgt")[1])
+
+            correctEuroparl.write(preprocess(example["trg"]["correct"][1],"tgt")[0])
+            correctOpensub.write(preprocess(example["trg"]["correct"][1],"tgt")[])
+
         examples.append(ccexamples[i]["type"])
